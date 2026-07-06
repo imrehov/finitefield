4302fc8039027fe90d83b8c9a6763d1fcade5ef5
imrehov <114602851+imrehov@users.noreply.github.com>
2026-07-06 14:06:02 +0200

    cyc conv vectorized

--- include/cyc_conv.hpp
+++ include/cyc_conv.hpp
@@ -3,12 +3,35 @@
 
 #include <array>
 #include <cstddef>
+#include <stdexcept>
+#include <vector>
 
-//y[k % N] = sum_i^k a[i] * b[k - i]
+// y[k] = sum_i a[i] * b[(k - i) mod N]
 template<typename T, std::size_t N>
 std::array<T, N> cyc_conv(const std::array<T, N> &a, const std::array<T, N> &b) {
     
     std::array<T, N> result{};
+
+    for (std::size_t i = 0; i < N; ++i) {
+        for (std::size_t j = 0; j < N; ++j) {
+            result[(i + j) % N] += a[i] * b[j];
+        }
+    }
+
+    return result;
+};
+
+template<typename T>
+std::vector<T> cyc_conv(const std::vector<T> &a, const std::vector<T> &b) {
+    
+    if (a.size() != b.size()) {
+        throw std::invalid_argument("cyclic convolution needs equal sizes");
+    }
+
+    std::size_t N = a.size();
+
+    //initialize a result vector with N elements already so we dont have to add them in the loop
+    std::vector<T> result(N, T{});
 
     for (std::size_t i = 0; i < N; ++i) {
         for (std::size_t j = 0; j < N; ++j) {
--- tests/test_cyc_conv.cpp
+++ tests/test_cyc_conv.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <stdexcept>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../src/doctest.hpp"
 
@@ -25,3 +26,43 @@ TEST_CASE("cyclic convolution works with size 1") {
 
     CHECK(result == expected);
 }
+
+TEST_CASE("cyc conv vector 1") {
+    std::vector<int> a{1, 2, 3, 4};
+    std::vector<int> b{5, 6, 7, 8};
+
+    auto result = cyc_conv(a, b);
+
+    std::vector<int> expected{
+        66, // 1*5 + 2*8 + 3*7 + 4*6
+        68, // 1*6 + 2*5 + 3*8 + 4*7
+        66, // 1*7 + 2*6 + 3*5 + 4*8
+        60  // 1*8 + 2*7 + 3*6 + 4*5
+    };
+
+    CHECK(result == expected);
+}
+
+TEST_CASE("cyc conv vector empty") {
+    std::vector<int> a{0, 0, 0, 0};
+    std::vector<int> b{0, 0, 0, 0};
+
+    auto result = cyc_conv(a, b);
+
+    std::vector<int> expected{
+        0, // 1*5 + 2*8 + 3*7 + 4*6
+        0, // 1*6 + 2*5 + 3*8 + 4*7
+        0, // 1*7 + 2*6 + 3*5 + 4*8
+        0  // 1*8 + 2*7 + 3*6 + 4*5
+    };
+
+    CHECK(result == expected);
+}
+
+TEST_CASE("cyc conv vector diff size") {
+    std::vector<int> a{0, 0, 0, 0, 0};
+    std::vector<int> b{0, 0, 0, 0};
+
+    CHECK_THROWS_AS(cyc_conv(a, b), std::invalid_argument);
+}
+
