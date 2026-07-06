8fc045924b02ac42cd27c5d1ebea8f2536512eab
imrehov <114602851+imrehov@users.noreply.github.com>
2026-07-06 15:02:28 +0200

    intt naive slow vectorized

--- include/intt_naive_slow.hpp
+++ include/intt_naive_slow.hpp
@@ -4,6 +4,7 @@
 #include "mod_t.hpp"
 #include <array>
 #include <cstddef>
+#include <vector>
 
 //the only thing that changes is that we need to multiply the sum with n^-1 and take omega^-k * j
 template<int RootOfUnity, int M, std::size_t N>
@@ -19,6 +20,32 @@ std::array<mod_t<M>, N> intt_naive_slow(const std::array<mod_t<M>, N> &a) {
 
     for (std::size_t k = 0; k < N; ++k) {
         result[k] = mod_t<M>{0};
+
+        for (std::size_t j = 0; j < N; ++j) {
+            result[k] += a[j] * omega_inv.exp(static_cast<int>(j * k));
+        }
+        //need to multiply with the n^-1 scaling factor
+        result[k] *= n_inv;
+    }
+
+    return result;
+}
+
+//the only thing that changes is that we need to multiply the sum with n^-1 and take omega^-k * j
+template<int RootOfUnity, int M>
+std::vector<mod_t<M>> intt_naive_slow(const std::vector<mod_t<M>> &a) {
+    const std::size_t N = a.size();
+    
+    std::vector<mod_t<M>> result(N, mod_t<M>{});
+
+    mod_t<M> omega{RootOfUnity};
+
+    //cant use -k because unsigned
+    mod_t<M> omega_inv = omega.exp(-1);
+
+    mod_t<M> n_inv = mod_t<M>{static_cast<int>(N)}.exp(-1);
+
+    for (std::size_t k = 0; k < N; ++k) {
 
         for (std::size_t j = 0; j < N; ++j) {
             result[k] += a[j] * omega_inv.exp(static_cast<int>(j * k));
--- tests/test_intt_naive_slow.cpp
+++ tests/test_intt_naive_slow.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <vector>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../src/doctest.hpp"
 
@@ -30,6 +31,40 @@ TEST_CASE("inverse NTT works for known transformed vector") {
     auto recovered = intt_naive_slow<4, 17, 4>(transformed);
 
     std::array<mod_t<17>, 4> expected{
+        mod_t<17>{1},
+        mod_t<17>{2},
+        mod_t<17>{3},
+        mod_t<17>{4}
+    };
+
+    CHECK(recovered == expected);
+}
+
+TEST_CASE("inverse NTT reverses forward NTT vec") {
+    std::vector<mod_t<17>> a{
+        mod_t<17>{1},
+        mod_t<17>{2},
+        mod_t<17>{3},
+        mod_t<17>{4}
+    };
+
+    auto transformed = ntt_naive_slow<4, 17>(a);
+    auto recovered = intt_naive_slow<4, 17>(transformed);
+
+    CHECK(recovered == a);
+}
+
+TEST_CASE("inverse NTT works for known transformed vector vec") {
+    std::vector<mod_t<17>> transformed{
+        mod_t<17>{10},
+        mod_t<17>{7},
+        mod_t<17>{15},
+        mod_t<17>{6}
+    };
+
+    auto recovered = intt_naive_slow<4, 17>(transformed);
+
+    std::vector<mod_t<17>> expected{
         mod_t<17>{1},
         mod_t<17>{2},
         mod_t<17>{3},
