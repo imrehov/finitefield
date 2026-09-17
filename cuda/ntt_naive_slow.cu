61a8ad0a6050b4b949d61c74c60ae39183cb2413
imrehov <114602851+imrehov@users.noreply.github.com>
2026-09-17 16:48:39 +0200

    first cuda stuff

--- cuda/ntt_naive_slow.cu
+++ cuda/ntt_naive_slow.cu
@@ -1,54 +1,72 @@
-#include <__clang_cuda_runtime_wrapper.h>
-#include <cuda_runtime_api.h>
 #include <memory.h>
 #include <cstdlib>
 #include <ctime>
 #include <numeric>
-#include <stdio.h>
+
+#include <cuda_runtime_api.h>
 #include <cuda/cmath>
 
 #include "../include/mod_t.hpp"
 
+template <int M>
+__global__ void ntt_naive_slow_cuda(const mod_t<M>* A, mod_t<M>* C, const mod_t<M> omega, const int n){
+    auto workIndex {threadIdx.x + blockIdx.x * blockDim.x};
 
-__global__ void ntt_naive_slow_cuda(){
-
+    if (workIndex < n){
+        for (std::size_t i {0}; i < n; ++i) {
+            C[workIndex] += A[i] * omega.exp(static_cast<int>(workIndex * i));        
+        }
+    }
 }
 
 // param n is the length of the vector, idk might be btter option to include it
 template <int RootOfUnity, int M>
-void ntt_naive_slow_cuda_wrapper(int n){
+void ntt_naive_slow_cuda_wrapper(const int n){
+    
+    constexpr mod_t<M> omega {RootOfUnity};
+
+    const auto length {n * sizeof(mod_t<M>)};
+    
     mod_t<M>* A {nullptr};
-    mod_t<M>* B {nullptr};
     mod_t<M>* C {nullptr};
 
     mod_t<M>* devA {nullptr};
-    mod_t<M>* devB {nullptr};
     mod_t<M>* devC {nullptr};
 
-    cudaMallocHost(&A, n * sizeof(mod_t<M>));
-    cudaMallocHost(&B, n * sizeof(mod_t<M>));
-    cudaMallocHost(&C, n * sizeof(mod_t<M>));
+    cudaMallocHost(&A, length);
+    cudaMallocHost(&C, length);
 
-    fillAllocatedArray(A);
-    fillAllocatedArray(B);
+    fillAllocatedArray(A, n);
 
-    cudaMalloc(&devA, n * sizeof(mod_t<M>));
-    cudaMalloc(&devB, n * sizeof(mod_t<M>));
-    cudaMalloc(&devC, n * sizeof(mod_t<M>));
+    cudaMalloc(&devA, length);
 
-    cudaMemcpy(devA, A, n * sizeof(mod_t<M>), cudaMemcpyHostToDevice);
-    cudaMemcpy(devB, B, n * sizeof(mod_t<M>), cudaMemcpyHostToDevice);
-    cudaMemset(devC, 0, n * sizeof(mod_t<M>));
+    cudaMalloc(&devC, length);
 
-    int threads {256};
-    //TODO where is this from
-    int blocks {cuda::ceil_div(n, threads)};
+    cudaMemcpy(devA, A, length, cudaMemcpyHostToDevice);
+    cudaMemset(devC, 0, length);
 
-    //TODO finish
+    constexpr int threads {256};
+
+    //this is basically (n + threads - 1) / threads
+    const int blocks {cuda::ceil_div(n, threads)};
+
+    ntt_naive_slow_cuda<M><<<blocks, threads>>>(devA, devC, n);
+
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(C, devC, length, cudaMemcpyDeviceToHost);
+
+    //cleanup
+    cudaFree(devA);
+    cudaFree(devC);
+    cudaFreeHost(A);
+    cudaFreeHost(C);
+
+    
 }
 template <int M>
 void fillAllocatedArray(mod_t<M>* a, int len){
-    std::iota(std::begin(a), std::end(a), 1);
+    std::iota(a, a + len, mod_t<M>{1});
 
 }
 
